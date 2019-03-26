fa74f54fdad419f0b1e0f4aa5f42b0416a2cc116
nfrd <vincent.nafrada@mail.utoronto.ca>
2019-03-26 18:18:28 -0400

    Added FaceDetector file, tested it with VideoReader in main

--- src/main.py
+++ src/main.py
@@ -3,15 +3,17 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import VideoReader
-
+import FaceDetector
 
 vr = VideoReader.VideoReader('../lib/Sample_1.mp4', sampleRate = 1)
+fd = FaceDetector.FaceDetector()
 
 # Skipping a minute in for testing
-for i in range(10):
-    vr.getNextFrame()
-
+for i in range(40):
+    frame = fd.detectFace(vr.getNextFrame())
+    
 # This displays it
 plt.figure()
 plt.axis('off')
-plt.imshow(vr.getNextFrame())
+plt.imshow(frame)
+plt.show()
