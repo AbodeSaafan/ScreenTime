bcae8bb1a67ab7d2043bd28f611fe011a46f84c3
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2019-03-27 13:04:44 -0400

    Adding face cluster file skeleton

---
+++ src/FaceCluster.py
@@ -0,0 +1,16 @@
+#https://www.pyimagesearch.com/2018/09/24/opencv-face-recognition/
+#https://www.cv-foundation.org/openaccess/content_cvpr_2015/app/1A_089.pdf
+#https://www.pyimagesearch.com/2018/07/09/face-clustering-with-python/
+
+
+'''
+This class is used to create the cluster of faces 
+'''
+class FaceCluster():
+    def __init__(self):
+        # Do stuff here like init vars we need
+        return 
+        
+    def addFaces(self, faces):
+        # Add to cluster
+        return 
